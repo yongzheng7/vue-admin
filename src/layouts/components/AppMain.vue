20557cb110a7ffd42211dd57c866695343122574
wyz5204 <42328880+yongzheng7@users.noreply.github.com>
2023-08-21 14:33:42 +0800

    Initial commit

---
+++ src/layouts/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { computed } from "vue"
+import { useRoute } from "vue-router"
+import { useTagsViewStore } from "@/store/modules/tags-view"
+
+const route = useRoute()
+const tagsViewStore = useTagsViewStore()
+
+const key = computed(() => {
+  // 返回 route.path 和 route.fullPath 有着不同的效果，大多数时候 path 更通用
+  return route.path
+})
+</script>
+
+<template>
+  <section class="app-main">
+    <div class="app-scrollbar">
+      <router-view v-slot="{ Component }">
+        <transition name="el-fade-in" mode="out-in">
+          <keep-alive :include="tagsViewStore.cachedViews">
+            <component :is="Component" :key="key" />
+          </keep-alive>
+        </transition>
+      </router-view>
+    </div>
+    <!-- 返回顶部 -->
+    <el-backtop />
+    <!-- 返回顶部（固定 Header 情况下） -->
+    <el-backtop target=".app-scrollbar" />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "@/styles/mixins.scss";
+
+.app-main {
+  width: 100%;
+  background-color: var(--v3-body-bg-color);
+}
+
+.app-scrollbar {
+  height: 100%;
+  overflow: auto;
+  @include scrollbar;
+}
+</style>
